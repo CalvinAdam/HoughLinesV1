bcf87f6ef1ecf6217b85d8e398e4b4af9ac787a2
DESKTOP-MQDQPF9\Calvin <calvinadams61@gmail.com>
2021-08-13 14:23:29 +0200

    Finds all lines over the threshold, but doesn't combine them

--- HoughLinesV1.py
+++ HoughLinesV1.py
@@ -1,6 +1,7 @@
 import cv2
 
-def HL_Calvin(img, threshold=15, min_line_length=30):  # The image must be grayscale
+
+def HL_Calvin(img, threshold=15, min_line_length=30, max_line_skip=1):  # The image must be grayscale
     horizontal_his = [0 for _ in range(len(img))]
     vertical_his = [0 for _ in range(len(img[0]))]
     lines_dict = {}
@@ -11,41 +12,30 @@ def HL_Calvin(img, threshold=15, min_line_length=30):  # The image must be grays
         for pixelnumber, pixel in enumerate(line):
             horizontal_his[linenumber] += not bool(pixel)
             vertical_his[pixelnumber] += not bool(pixel)
-    for his in [horizontal_his, vertical_his]:  # Combines lines
-        for index, line in enumerate(his):
-            if line == 0:
-                continue
-            else:
-                longest_line = 0
-                for thickness, hori_line in enumerate(his[index:]):
-                    if hori_line != 0:
-                        if hori_line >= longest_line:
-                            longest_line = hori_line
-                            longest_line_index = index + thickness
-                        elif hori_line < longest_line/4:
-                            his[index+thickness] = 0
-                            break
-                        his[index+thickness] = 0
-                    else:
-                        break
-                his[longest_line_index] = longest_line
     #  Eliminates the zero values but keeps the indices of the nonzero values
-    lines_dict['horizontal'] = {i: x for i, x in enumerate(horizontal_his) if x != 0}
-    lines_dict['vertical'] = {i: x for i, x in enumerate(vertical_his) if x != 0}
+    lines_dict['horizontal'] = {i: x for i, x in enumerate(horizontal_his) if x > threshold}
+    lines_dict['vertical'] = {i: x for i, x in enumerate(vertical_his) if x > threshold}
     # Finds the horizontal and vertical lines
     for lineindex, linevalue in lines_dict['horizontal'].items():
         img_line = img[lineindex]
         pixelindex = 0
+        current_line_value = linevalue
         while pixelindex < len(img_line):
             pixel = img_line[pixelindex]
             if pixel == 0:
+                line_skip = max_line_skip
                 for lengthindex, length in enumerate(img_line[pixelindex:]):
                     if length == 255:
-                        break
+                        line_skip -= 1
+                        if line_skip <= 0:
+                            break
+                    else:
+                        line_skip = max_line_skip
                 if lengthindex > min_line_length:
                     line = [pixelindex, lineindex, pixelindex+lengthindex, lineindex]
                     lines.append(line)
-                if lengthindex > linevalue * 0.85:
+                    current_line_value -= lengthindex
+                if current_line_value < min_line_length:
                     break
                 else:
                     pixelindex = pixelindex+lengthindex
@@ -54,16 +44,23 @@ def HL_Calvin(img, threshold=15, min_line_length=30):  # The image must be grays
     for columnindex, columnvalue in lines_dict['vertical'].items():
         img_column = img[:, columnindex]
         pixelindex = 0
+        current_column_value = columnvalue
         while pixelindex < len(img_column):
             pixel = img_column[pixelindex]
             if pixel == 0:
+                line_skip = max_line_skip
                 for lengthindex, length in enumerate(img_column[pixelindex:]):
                     if length == 255:
-                        break
+                        line_skip -= 1
+                        if line_skip <= 0:
+                            break
+                    else:
+                        line_skip = max_line_skip
                 if lengthindex > min_line_length:
                     column = [columnindex, pixelindex, columnindex, pixelindex+lengthindex]
                     lines.append(column)
-                if lengthindex > columnvalue * 0.85:
+                    current_column_value -= lengthindex
+                if current_column_value < min_line_length:
                     break
                 else:
                     pixelindex = pixelindex+lengthindex
@@ -73,13 +70,13 @@ def HL_Calvin(img, threshold=15, min_line_length=30):  # The image must be grays
     #  Adds the lines to the image
     for line in lines:
         x1, y1, x2, y2 = line
-        cv2.line(img, (x1, y1), (x2, y2), (0, 128, 0), 5)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 128, 0), 1)
     return lines, img
 
 
 if __name__ == "__main__":
-    img = cv2.imread("document", 0)
-    lines, img = HL_Calvin(img, 500)
+    img = cv2.imread("C:/Users/Calvin/find_lines/preprocessed_documents/L1-2010-1.tif", 0)
+    lines, img = HL_Calvin(img, threshold=50, max_line_skip=3)
     cv2.imshow('Image with lines', img)
     cv2.imwrite('linesdoc.jpg', img)
     cv2.waitKey(0)
